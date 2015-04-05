aa9688baae7056c0256edde204e5df7fc13b745c
ddafaster <daruin2004@hotmail.com>
2015-04-04 22:33:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ function start(route, handle){
 		
 		
 	}
-	http.createServer(onRequest).listen(86);
+	var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+	var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	http.createServer(onRequest).listen(port, ipaddress);
 	console.log('Server has started');
 }
 
